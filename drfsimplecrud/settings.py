2e612831421f617a86e5207f544f8c82773c695c
Eduardo Noguera <eduardo.noguera@gmail.com>
2023-12-07 15:13:08 -0300

    Actualizar configuración de la base de datos para Render y uso del txt

--- drfsimplecrud/settings.py
+++ drfsimplecrud/settings.py
@@ -82,11 +82,10 @@ WSGI_APPLICATION = 'drfsimplecrud.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_URL'),
-        conn_max_age=600
-    ),
-    'ENGINE': 'django.db.backends.postgresql',
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / "db.sqlite3",
+    }
 }
 
 
